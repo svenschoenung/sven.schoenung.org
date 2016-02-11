3b066993a9991ecf1da7e1efdccd3c5703966ad3
Sven Schoenung <sven.schoenung@gmail.com>
2016-02-11 22:13:58 +0100

    Add watch task and refactor Gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,44 +7,55 @@ const hasher = require('gulp-hasher');
 const buster = require('gulp-cache-buster');
 const webserver = require('gulp-webserver');
 
-const SRC = 'src'
-const DEST = 'www';
+const PATH = {};
+PATH.SRC = 'src';
+PATH.DEST = 'www';
+PATH.HTML = PATH.SRC + '/**/*.html';
+PATH.CSS = PATH.SRC + '/**/*.css';
+PATH.IMAGES = PATH.SRC + '/images/**/*';
 
 function cachebust() {
   return buster({
-    assetRoot: path.join(__dirname, DEST),
+    assetURL: '/',
+    assetRoot: path.join(__dirname, PATH.DEST),
     hashes: hasher.hashes,
-    hashLength: 32,
+    hashLength: 32, hashParam: 'hash',
   });
 }
 
 gulp.task('html', ['css', 'images'], () => {
-  return gulp.src(SRC+'/**/*.html')
+  return gulp.src(PATH.HTML)
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(cachebust())
-    .pipe(gulp.dest(DEST));
+    .pipe(gulp.dest(PATH.DEST));
 });
 
 gulp.task('css', ['images'], () => {
-  return gulp.src(SRC+'/**/*.css')
+  return gulp.src(PATH.CSS)
     .pipe(cssnano())
     .pipe(cachebust())
-    .pipe(gulp.dest(DEST))
+    .pipe(gulp.dest(PATH.DEST))
     .pipe(hasher());
 });
 
 gulp.task('images', () => {
-  return gulp.src(SRC+'/images/**/*', { base: SRC })
+  return gulp.src(PATH.IMAGES, { base: PATH.SRC })
     .pipe(imagemin({optimizationLevel:3}))
-    .pipe(gulp.dest(DEST))
+    .pipe(gulp.dest(PATH.DEST))
     .pipe(hasher());
 });
 
 gulp.task('default', ['html']);
 
 gulp.task('serve', () => {
-  return gulp.src(DEST)
+  return gulp.src(PATH.DEST)
     .pipe(webserver({
       livereload: true,
     }));
+});
+
+gulp.task('watch', () => {
+  gulp.watch(PATH.HTML, ['html']);
+  gulp.watch(PATH.CSS, ['css']);
+  gulp.watch(PATH.IMAGES, ['images']);
 });
