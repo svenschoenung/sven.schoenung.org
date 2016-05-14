6f15bf90253a19597643f8000377bb28bfa43573
Sven Schoenung <sven.schoenung@gmail.com>
2016-05-14 20:11:08 +0200

    Use gulp-rev-all for cache busting

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,9 +3,8 @@ const gulp = require('gulp');
 const htmlmin = require('gulp-htmlmin');
 const minifyCss = require('gulp-minify-css');
 const imagemin = require('gulp-imagemin');
-const hasher = require('gulp-hasher');
-const buster = require('gulp-cache-buster');
 const webserver = require('gulp-webserver');
+const RevAll = require('gulp-rev-all');
 
 const PATH = {};
 PATH.SRC = 'src';
@@ -15,43 +14,43 @@ PATH.CSS = PATH.SRC + '/**/*.css';
 PATH.IMAGES = PATH.SRC + '/images/**/*';
 PATH.GPG = PATH.SRC + '/**/public.gpg';
 
-function cachebust() {
-  return buster({
-    assetURL: '/',
-    assetRoot: path.join(__dirname, PATH.DEST),
-    hashes: hasher.hashes,
-    hashLength: 32, hashParam: 'hash',
-  });
-}
-
 gulp.task('gpg', () => {
   return gulp.src(PATH.GPG)
     .pipe(gulp.dest(PATH.DEST));
 });
 
-gulp.task('html', ['css', 'images'], () => {
+gulp.task('html', () => {
   return gulp.src(PATH.HTML)
     .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(cachebust())
     .pipe(gulp.dest(PATH.DEST));
 });
 
-gulp.task('css', ['images'], () => {
+gulp.task('css', () => {
   return gulp.src(PATH.CSS)
     .pipe(minifyCss())
-    .pipe(cachebust())
     .pipe(gulp.dest(PATH.DEST))
-    .pipe(hasher());
 });
 
 gulp.task('images', () => {
   return gulp.src(PATH.IMAGES, { base: PATH.SRC })
     .pipe(imagemin({optimizationLevel:3}))
     .pipe(gulp.dest(PATH.DEST))
-    .pipe(hasher());
 });
 
-gulp.task('default', ['html', 'gpg']);
+gulp.task('www', ['html', 'gpg', 'css', 'images']);
+
+gulp.task('dist', ['www'], () => {
+  const revAll = new RevAll({
+    dontRenameFile: [/\.html$/, /\.gpg$/],
+    dontUpdateReference: [/\.html$/, /\.gpg$/],
+    dontSearchFile: [/\.png$/, /\.jpg$/, /\.gpg$/]
+  });
+  return gulp.src(PATH.DEST + '/**')
+    .pipe(revAll.revision())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['dist']);
 
 gulp.task('serve', () => {
   return gulp.src(PATH.DEST)
@@ -67,4 +66,4 @@ gulp.task('watch', () => {
   gulp.watch(PATH.IMAGES, ['images']);
 });
 
-gulp.task('dev', ['default', 'serve', 'watch' ]);
+gulp.task('dev', ['www', 'serve', 'watch' ]);
