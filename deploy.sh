ed3d7e41226142b414ed717953c1ce734fa27f21
Sven Schoenung <sven.schoenung@gmail.com>
2016-05-15 11:02:03 +0200

    Add logging to deployment script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,11 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+echo "Deleting old deployment directory"
 sshpass -e ssh $SSHUSER@schoenung.org "rm -rf /var/www/schoenung.org-deploy"
+
+echo "Copying deployment directory to server"
 sshpass -e scp -r dist $SSHUSER@schoenung.org:/var/www/schoenung.org-deploy
+
+echo "Running deployment script on server"
 sshpass -e ssh $SSHUSER@schoenung.org "./deploy.sh schoenung.org"
