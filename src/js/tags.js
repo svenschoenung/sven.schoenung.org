8f85c8ebcd69f011dd8ca6b14ca385e32956a0a7
Sven Schoenung <sven.schoenung@gmail.com>
2017-03-01 19:26:12 +0100

    Generate tags dynamically

--- src/js/tags.js
+++ src/js/tags.js
@@ -1,27 +1,41 @@
-$('.tag').click(function() {
-  var $tag = $(this);
-  var $section = $tag.closest('.section');
-  var $tiles = $section.find('.tile');
-  var $tags = $section.find('.tag');
+(function() {
+  var $tags = [
+    'all',
+    'crypto', 'docs', 'games', 'images',
+    'news', 'music', 'code', 'social'
+  ].map(function(tag) {
+    var $tag = $('<span class="tag" data-tag="' + tag + '"><i class="fa fa-tag"></i>' + tag + '</span>');
 
-  $tags.removeClass('active');
-  $tag.addClass('active');
-
-  var activeTag = $section.find('.tag.active').attr('data-tag');
-
-  if (activeTag.length == 0) {
-    $tiles.removeClass('hidden');
-    return;
-  }
-
-  $tiles.each(function(i, tile) {
-    var $tile = $(tile);
-    var tileTags = $tile.attr('data-tags').split(/ /);
-    if ($.inArray(activeTag, tileTags) >= 0) {
-      $tile.removeClass('hidden');
-    } else {
-      $tile.addClass('hidden');
-    }
+    $tag.click(function() {
+      var $section = $tag.closest('.section');
+      var $tiles = $section.find('.tile');
+      var $tags = $section.find('.tag');
+    
+      $tags.removeClass('active');
+      $tag.addClass('active');
+    
+      var activeTag = $section.find('.tag.active').attr('data-tag');
+    
+      if (activeTag.length == 0) {
+        $tiles.removeClass('hidden');
+        return;
+      }
+    
+      $tiles.each(function(i, tile) {
+        var $tile = $(tile);
+        var tileTags = $tile.attr('data-tags').split(/ /);
+        if ($.inArray(activeTag, tileTags) >= 0) {
+          $tile.removeClass('hidden');
+        } else {
+          $tile.addClass('hidden');
+        }
+      });
+    });
+    return $tag;
   });
 
-});
+  $tags[0].addClass('active');
+  $tags[0].attr('data-tag', '');
+
+  $('.tags').append($tags);
+})();
