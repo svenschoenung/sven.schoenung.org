13a85beb5ee6f7f70ac7f1ecb94f15f6a6b5a21b
Sven Schoenung <sven.schoenung@gmail.com>
2018-07-19 19:53:03 +0200

    Show gray blocks instead of text until fonts are loaded

--- src/js/email.js
+++ src/js/email.js
@@ -2,5 +2,5 @@
   var email = "twfoAtdipfovoh/psh".replace(/./g, function(chr) {
     return String.fromCharCode(chr.charCodeAt(0) - 1);
   });
-  $('.email').append('<i class="fa fa-envelope"></i><a href="' + email + '" class="email">' + email + ' </a>');
+  $('.email').html('<a href="' + email + '" class="email">' + email + ' </a>');
 })();
