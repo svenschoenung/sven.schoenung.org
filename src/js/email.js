7f42db026f3eae5ad1c8fe9b2f28e14c4ac6a744
Sven Schoenung <sven.schoenung@gmail.com>
2018-07-19 20:29:33 +0200

    Add projects section

--- src/js/email.js
+++ src/js/email.js
@@ -2,5 +2,5 @@
   var email = "twfoAtdipfovoh/psh".replace(/./g, function(chr) {
     return String.fromCharCode(chr.charCodeAt(0) - 1);
   });
-  $('.email').html('<a href="' + email + '" class="email">' + email + ' </a>');
+  $('.email').html('<a href="' + email + '" class="email"><span>' + email + '</span></a>');
 })();
